9965077d7c3c8d1a4ec2bc5699a0856e639a399d
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-05-29 10:23:05 +0300

    fixed server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var mqtt = require('mqtt');
-var fs = require('fs');
+var mqtt = require('mqtt'); // Підключення бібліотеки mqtt сервера
+var fs = require('fs'); 
 
-var content = fs.readFileSync('mqtt_credentials.json');
-var mqttCredentials = JSON.parse(content);
+var content = fs.readFileSync('mqtt_credentials.json'); // Підключення масиву з правами доступу 
+var mqttCredentials = JSON.parse(content); // Перетворення JSON - об'єкту
 
+var mqttOptions = {
+    clientId: mqttCredentials.clientId,
+    host: mqttCredentials.host,
+    port: mqttCredentials.port,
+    username: mqttCredentials.username,
+    password: mqttCredentials.password
+};
+
+var client = mqtt.connect(mqttOptions); // Підключення до серверу
+
+client.on('connect', function(){ // Функція викликається в разі успішного встановлення з'єднання
+    console.log('Server successfully start!')
+})
