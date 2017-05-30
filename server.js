4b8dcd86ae42efe13afbc47f9d9ca5a8d45a3403
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-05-30 20:55:23 +0300

    mqtt. part 1

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var fs = require('fs');
 var content = fs.readFileSync('mqtt_credentials.json'); // Підключення масиву з правами доступу 
 var mqttCredentials = JSON.parse(content); // Перетворення JSON - об'єкту
 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 var mqttOptions = {
     clientId: mqttCredentials.clientId,
     host: mqttCredentials.host,
@@ -15,5 +20,17 @@ var mqttOptions = {
 var client = mqtt.connect(mqttOptions); // Підключення до серверу
 
 client.on('connect', function(){ // Функція викликається в разі успішного встановлення з'єднання
-    console.log('Server successfully start!')
-})
+    console.log('Server successfully start!');
+    client.subscribe('temp');
+    client.subscribe('weight');
+});
+
+client.on('message', function (topic, message) {
+        if(topic==='temp'){
+            console.log('Temp: ' + message);
+        }
+        else if(topic==='weight'){
+            console.log('Weight: ' + message);
+        }
+});
+
